9cb07235d2d66d9378a9090bd9aeadc227bca83a
Twinkle Rana <startwinklerana@gmail.com>
2026-02-24 10:17:23 +0530

    react fundamentals-1

---
+++ react-revise/src/App.jsx
@@ -0,0 +1,27 @@
+import {useState } from 'react';
+function App() {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  function reset() {
+    setCount(0);
+  }
+  return (
+    <>
+    <h1> Counter App</h1>
+    <h2>{count}</h2>
+    <button onClick={increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={reset}>Reset</button>
+    </>
+
+  )
+}
+export default App;
